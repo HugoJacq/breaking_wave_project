6f0bcbfbc390bf91918b245ceccfde62cac41212
HugoJacq <hjacquet974@gmail.com>
2025-10-13 14:20:20 +0200

    input gen file creation

---
+++ gen_inputs_fields/input_generator.py
@@ -0,0 +1,13 @@
+"""
+A generator of initial conditions for basilisk multilayer model
+
+From a spectra, we compute eta (surface elevation) and the ocean currents (3D).
+
+Hypothesis :
+    - linear theory
+    - modes have random phases
+
+
+Author : Hugo Jacquet, october 2025
+    from a code by Jiarong Wu (@jiarong-wu)
+"""
---
+++ gen_inputs_fields/tools.py
@@ -0,0 +1,6 @@
+"""
+Tool box
+
+Author : Hugo Jacquet, october 2025
+    based on tools by Jiarong Wu (@jiarong-wu)
+"""
