4521b1329ee51e545b9f93ce92260a3d7ee71af7
Hugo JACQUET <hugo.jacquet1@univ-grenoble-alpes.fr>
2026-03-20 08:20:51 +0100

    sync verif_spectrum

--- sync_sandbox.sh
+++ sync_sandbox.sh
@@ -1,7 +1,18 @@
 #/bin/bash
+#
+# add -n for dry run
+#
+# run with ./sync_sandbox.sh
+
+# Jiarong sim
 INPUT=$MYSANDBOX/hugoj/reproducing_jiarongs_plots/
 SUBDIR="postprocess/"
 OUTPUT=a0_jiarong_simu/
-
-# add -n for dry run
 rsync -av --include='*/' --include='*.py' --include='*.c' --include='*.txt' --exclude='*' $INPUT $OUTPUT
+rsync -av --include='*/' --include='*.py' --include='*.c' --include='*.txt' --exclude='*' $OUTPUT $INPUT
+
+# verif_spectrum
+INPUT="verif_spectrum/"
+OUTPUT=$MYSANDBOX/hugoj/verif_spectrum/
+rsync -av --include='*/' --include='*.py' --include='*.md' --include='*.txt' --include='Makefile' --exclude='*' $INPUT $OUTPUT
+rsync -av --include='*/' --include='*.py' --include='*.md' --include='*.txt' --include='Makefile' --exclude='*' $OUTPUT $INPUT
